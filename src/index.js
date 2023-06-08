572aef9f0b571fb09a882ef187eed812a45eaed4
dwhale2 <devinwaynehale@gmail.com>
2023-06-08 16:37:33 -0500

    Began search bar dom logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import forecast from "./modules/logic/weatherAPI";
 import { currentWeather, hourlyWeather, sevenDayWeather } from "./modules/logic/dataParse";
 import search from "./modules/logic/search";
+import searchInitialize from "./modules/dom/searchbar";
+
+searchInitialize();
---
+++ src/modules/dom/searchbar.js
@@ -0,0 +1,10 @@
+const searchInitialize = () => {
+    const searchBar = document.getElementById('searchBar')
+
+    searchBar.addEventListener('change', event => {
+       console.log('Yeah!')
+        
+    })
+}
+
+export default searchInitialize;
