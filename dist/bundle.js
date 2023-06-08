b233fe1a2062a1966bb661e7adf9e95cfe01acf0
dwhale2 <devinwaynehale@gmail.com>
2023-06-08 16:32:57 -0500

    Added search bar in HTML

--- dist/bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";(async a=>{const c=await fetch("http://api.weatherapi.com/v1/search.json?key=0e45ac1c51a149518c7205713230706&q=London&aqi=no",{mode:"cors"});await c.json()})()})();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import forecast from "./modules/logic/weatherAPI";
 import { currentWeather, hourlyWeather, sevenDayWeather } from "./modules/logic/dataParse";
 import search from "./modules/logic/search";
-
-search('London');
