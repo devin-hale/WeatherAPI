fbf12c83b5261cf04ab3af5e1f4c43ffb0cda767
dwhale2 <devinwaynehale@gmail.com>
2023-06-12 10:41:29 -0500

    Autocomplete searchbar functioning

--- src/modules/dom/searchbar.js
+++ src/modules/dom/searchbar.js
@@ -1,9 +1,23 @@
+import search from "../logic/search"
+
+
 const searchInitialize = () => {
     const searchBar = document.getElementById('searchBar')
 
-    searchBar.addEventListener('change', event => {
-       console.log('Yeah!')
-        
+    searchBar.addEventListener('input', async(event) => {
+        if (event.target.value.length > 0) {
+            document.getElementById('searchResults').textContent = '';
+            console.log('Input')
+            let searchResult = await search(event.target.value);
+            searchResult.forEach(result => {
+                let resultDiv = document.createElement('p');
+                resultDiv.innerHTML = `${result.name}, ${result.region}`;
+                resultDiv.id = result.id;
+                resultDiv.classList = 'resultDiv'
+                document.getElementById('searchResults').appendChild(resultDiv);
+            })
+            return
+        }
     })
 }
 
