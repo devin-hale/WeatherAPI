b8fd842cac5d2a81aaca924e9adab1db337f99ba
dwhale2 <devinwaynehale@gmail.com>
2023-06-15 12:51:50 -0500

    Weekly div functions, needs styling

--- src/modules/dom/render7Day.js
+++ src/modules/dom/render7Day.js
@@ -3,6 +3,32 @@ import { sevenDayWeather } from "../logic/dataParse"
 const render7Day = (forecastData) => {
     let sevenDayData = sevenDayWeather(forecastData);
     console.log(sevenDayData);
+
+    const weeklyContainer = document.getElementById('weeklyWeather')
+
+    sevenDayData.forEach( obj => {
+
+        let dayDiv = document.createElement('div');
+        dayDiv.classList = 'dayDiv';
+        
+        let dayDivWD = document.createElement('p');
+        dayDivWD.classList = 'dayDivWD'
+        dayDivWD.innerHTML = obj.day;
+        dayDiv.appendChild(dayDivWD);
+
+        let weekCon = document.createElement('img');
+        weekCon.classList = 'weekCon';
+        weekCon.src = obj.condition.icon;
+        dayDiv.appendChild(weekCon);
+
+        let weekTemp = document.createElement('p');
+        weekTemp.classList = 'weekTemp';
+        weekTemp.innerHTML = obj.temp_f;
+        dayDiv.appendChild(weekTemp);
+
+        weeklyContainer.appendChild(dayDiv);
+
+    })
 }
 
 export default render7Day;
