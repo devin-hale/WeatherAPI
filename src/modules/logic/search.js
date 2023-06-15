6521ed3ae8a26b6ed236b17163cd00ead19d6658
dwhale2 <devinwaynehale@gmail.com>
2023-06-15 16:43:07 -0500

    Changed to secure HTTPS requests

--- src/modules/logic/search.js
+++ src/modules/logic/search.js
@@ -1,7 +1,7 @@
 //Takes search term and finds locations from the Weather API matching the search term.
 
 const search = async(searchTerm) => {
-    const response = await fetch(`http://api.weatherapi.com/v1/search.json?key=0e45ac1c51a149518c7205713230706&q=${searchTerm}&aqi=no`,{mode: 'cors'})
+    const response = await fetch(`https://api.weatherapi.com/v1/search.json?key=0e45ac1c51a149518c7205713230706&q=${searchTerm}&aqi=no`,{mode: 'cors'})
     const locationResults = await response.json();
     return locationResults
 }
