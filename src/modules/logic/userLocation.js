403874491dbb0a9340d85eeba02506447b77131a
dwhale2 <devinwaynehale@gmail.com>
2023-06-16 11:59:21 -0500

    Added footer.  Corrected some issues with search results

--- src/modules/logic/userLocation.js
+++ src/modules/logic/userLocation.js
@@ -1,17 +1,20 @@
-
+import { hideLoading, displayLoading } from "../dom/loader";
 //gets user coordinates, returns in a format accepted by the weather API
 
 const getCoordinates = async () => {
     const successCB = (position) => {
+      hideLoading();
       return position.coords.latitude + ',' + position.coords.longitude;
     };
   
     try {
+      displayLoading();
       const position = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
       });
       return successCB(position);
     } catch (error) {
+      hideLoading();
       return null;
     }
   };
